952f46b339d949b452934e16ca8e1936a4c3680c
Eduardo Souza <edcampossouza@gmail.com>
2023-04-20 15:55:03 -0300

    feat: /fruit routes tests

--- tests/fruits.test.ts
+++ tests/fruits.test.ts
@@ -9,3 +9,35 @@ describe("GET /health", () => {
     expect(result.text).toBe("I'am alive!");
   });
 });
+
+describe("/fruits", () => {
+  it("should be empty on start", async () => {
+    const result = await server.get("/fruits");
+    expect(result.body).toEqual([]);
+  });
+
+  it("should return 201 on creation", async () => {
+    const result = await server
+      .post("/fruits")
+      .send({ name: "papaia", price: 20 });
+    expect(result.status).toEqual(201);
+  });
+
+  it("should return the created fruit", async () => {
+    const result = await server.get("/fruits");
+    expect(result.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ name: "papaia", price: 20 }),
+      ])
+    );
+    expect(result.body).toHaveLength(1);
+  });
+
+  it("should not allow duplicated names", async () => {
+    const result = await server
+      .post("/fruits")
+      .send({ name: "papaia", price: 20 });
+
+    expect(result.status).toBe(409);
+  });
+});
